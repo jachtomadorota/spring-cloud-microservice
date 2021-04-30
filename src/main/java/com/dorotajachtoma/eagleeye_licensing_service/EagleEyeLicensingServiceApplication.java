e724ad65e83122a5dd0495fcc3cacccbe78b1ad6
jachtomadorota <jachtoma.do@gmail.com>
2021-04-30 11:56:22 +0200

    initial commit

---
+++ src/main/java/com/dorotajachtoma/eagleeye_licensing_service/EagleEyeLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dorotajachtoma.eagleeye_licensing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyeLicensingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleEyeLicensingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dorotajachtoma/eagleeye_licensing_service/EagleEyeLicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorotajachtoma.eagleeye_licensing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleEyeLicensingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
